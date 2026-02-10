d874bad96f8bec62cca6c0eb8b926a14ab2a6edf
geekkid1 <captain.emily.blum@gmail.com>
2026-02-10 04:17:43 +0000

    Deploying to gh-pages from @ geekkid1/geekkid1.github.io@b0e5fe4194d59dc454d7b17e67d4c07a73c5ac45 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -80,16 +80,16 @@ ninja.data = [{
             window.location.href = "/pages/2025/anew/";
           
         },
-      },{id: "books-at-the-mountains-of-madness",
-          title: 'At The Mountains of Madness',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/frankenstein/";
-            },},{id: "books-the-hitchhikers-39-guide-to-the-galaxy",
+      },{id: "books-the-hitchhikers-39-guide-to-the-galaxy",
           title: 'The Hitchhikers&amp;#39; Guide to the Galaxy',
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/hitchhikers/";
+            },},{id: "books-frankenstein",
+          title: 'Frankenstein',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/frankenstein/";
             },},{id: "books-house-of-leaves",
           title: 'House of Leaves',
           description: "",
