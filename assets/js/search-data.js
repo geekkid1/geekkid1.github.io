dbbe69f60c7f7ade359bba18e782c621fd94abdf
geekkid1 <captain.emily.blum@gmail.com>
2026-05-20 02:36:53 +0000

    Deploying to gh-pages from @ geekkid1/geekkid1.github.io@2ce967db8f058a048397e0c8654866dc14a6ced7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -195,6 +195,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/men_at_arms/";
+            },},{id: "books-men-at-arms",
+          title: 'Men at Arms',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/feet_of_clay/";
             },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
